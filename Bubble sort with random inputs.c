fec993d816f0b9dde582b04280097b871b8d8255
PalyPvP <palkopelak@gmail.com>
2026-02-27 10:18:38 +0100

    Added max2min bubble sort

--- Bubble sort with random inputs.c
+++ Bubble sort with random inputs.c
@@ -1,6 +1,7 @@
 /*Okay, I don't want to get overworked so I'm going to 'only' do bubble sort. Nah, it's a big accomplishment, yep, no doubt.<delulu>*/
 /*Edit, okayyy, I went a bit overboard :3 , but I like it and I'm proud iof it. I was excited and curious, and that is IT. */
 /*NO WAAAY, HUGE FUCKING IMPROVEMENTS. me b4: pointer... what? me now: muhahahaha, I am master of it all. Like the comparison of my previous code and now is so fucking huge*/
+/*Pushed onto github, hell yeah!*/
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -10,7 +11,7 @@
 void check_i(int check, int n);  // declaration (promise) <-- ChatGPT ; this is just to tell the program that the function check_i exists, because array_init_manuals print_array was having some problems
 void print_array(int array[], int n_1);
 
-int n_1;//number of elements in array
+//int n_1;//number of elements in array //unnecessary global variable, I'm already always passing it to functions, replaced with local variable in main
 
 void array_init_gen(int array[], int a, int b, int n_1)
 {
@@ -60,6 +61,34 @@ void bubblesort_min2max(int array_bs[], int n_1)
     }
 }
 
+void bubblesort_max2min(int array[], int n_1)//I got the conclusion that the for loops should just stay the same and we will add one variable that changes over loops
+{//here I dont need to write the pointer thingy manually because C converts arrays to pointers automatically , so it updates the original value (it works with it too)
+    for (int i=0;i<n_1-1;i++ )//quick recap: if i is smaller than n_1-1 then go
+    {/*
+        printf("%d. pass O.F\t\t",i); //troubleshooting 1
+        print_array(array,n_1);
+        */
+        int k =n_1-1;//so for 5 element array the value is 4 (last elements line value)
+        for (int j=0;j<n_1-1-i;j++)
+        {/*
+            printf("\t%d. pass I.F.\t",j); //troubleshooting 1
+            print_array(array,n_1);
+            printf("\n");
+            */
+
+            //k = k-1; //we decrease it every pass instead of increasing like with j in min2max function ; I accidentally put it before executing part so it didnt sort the most-right variable
+            if (array[k]>array[k-1])
+            {
+                int temp = array[k-1];
+                array[k-1] = array[k];
+                array[k] = temp;
+            }
+            k = k-1;
+        }
+        //printf("\n"); //troubleshooting 1
+    }
+}
+
 
 void check_i (int check, int n)//n is just the number of things that you are scanning for
 {
@@ -85,6 +114,7 @@ void i_generation_range (int *a, int *b)//no wayy, I actually learned adresses a
 int main()
 {
     int choice_1,a,b,choice_2;
+    int n_1;
 
     srand(time(0));
 
@@ -120,19 +150,28 @@ int main()
         printf("ERROR: wrong input values (only '1' or '2' permitted)\n");
         exit(1);
     }
-    //int array_sorted_min2max[] = array[]; //wrong way to copy an array
-    int array_sorted_min2max[n_1];
-    memcpy(array_sorted_min2max, array, sizeof(array));
     if (choice_2 == 1)
     {
+        //int array_sorted_min2max[] = array[]; //wrong way to copy an array
+        int array_sorted_min2max[n_1];
+        memcpy(array_sorted_min2max, array, sizeof(array));//copies memory from one place to another e.g; (<destination>,<source>,<size of the load>)
+
         bubblesort_min2max(array_sorted_min2max,n_1);
+        printf("The original array:\t");
         print_array(array,n_1);
+        printf("The sorted array:\t");
         print_array(array_sorted_min2max,n_1);
 
     }
     else if (choice_2 == 2)
     {
-    
+        int array_sorted_max2min[n_1];
+        memcpy(array_sorted_max2min, array, sizeof(array));//copies memory from one place to another e.g; (<destination>,<source>,<size of the load>)
+        bubblesort_max2min(array_sorted_max2min, n_1);
+        printf("The original array:\t");
+        print_array(array,n_1);
+        printf("The sorted array:\t");
+        print_array(array_sorted_max2min,n_1);
     }
     /*Implement Bubble Sort yourself — you already have the array ready, it’s a natural next step.
 
